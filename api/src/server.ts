a0584953d144c82befb54eb99f36f7e1d47c4b62
yube01 <yubraja008@gmail.com>
2023-05-30 14:13:30 +0545

    api created

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { sample_foods, sample_tag } from "./data";
 
 
 
@@ -13,6 +14,34 @@ app.use(cors(
     }
 ))
 
+
+app.get("/api/foods",(req,res)=>{
+    res.send(sample_foods)
+})
+
+app.get("/api/foods/search/:searchTerms",(req,res)=>{
+    const searchTerms = req.params.searchTerms
+
+    const foods = sample_foods.filter(food=>food.name.toLowerCase().includes(searchTerms.toLowerCase()))
+    res.send(foods)
+})
+
+
+app.get("/api/foods/tags",(req,res)=>{
+    res.send(sample_tag)
+})
+
+app.get("/api/foods/tags/:tagName",(req,res)=>{
+    const tagName = req.params.tagName;
+    const foods = sample_foods.filter(food => food.tags?.includes(tagName))
+    res.send(foods)
+})
+
+app.get("/api/foods/:foodId",(req,res)=>{
+    const foodId =  req.params.foodId;
+    const food = sample_foods.find(food => food.id == foodId)
+    res.send(food)
+})
 
 app.listen(8000,()=>{
     console.log("Server started")
