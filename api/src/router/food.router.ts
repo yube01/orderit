aa1f8aa2827728b062e5a460228860df97fd2189
yube01 <yubraja008@gmail.com>
2023-06-03 20:42:48 +0545

    data pushed to mongodb

--- api/src/router/food.router.ts
+++ api/src/router/food.router.ts
@@ -1,36 +1,80 @@
 import Router from "express"
 import { sample_foods, sample_tag } from "../data"
-
+import ascyncHandler from "express-async-handler"
+import { FoodModel } from "../models/food.model"
 const router = Router()
 
 
-router.get("/",(req,res)=>{
-    res.send(sample_foods)
-})
+router.get("/seed",ascyncHandler(
+    async(req,res)=>{
+        const foodCounts = await FoodModel.countDocuments()
 
-router.get("/search/:searchTerms",(req,res)=>{
-    const searchTerms = req.params.searchTerms
+        if(foodCounts>0)
+        {
+            res.send("Seed is already done")
+            return
+        }
 
-    const foods = sample_foods.filter(food=>food.name.toLowerCase().includes(searchTerms.toLowerCase()))
+        await FoodModel.create(sample_foods)
+        res.send("Seed is done")
+    }
+  
+))
+
+
+router.get("/",ascyncHandler(async(req,res)=>{
+    const foods = await FoodModel.find()
     res.send(foods)
-})
+}))
 
+router.get("/search/:searchTerms",ascyncHandler(async(req,res)=>{
 
-router.get("/tags",(req,res)=>{
-    res.send(sample_tag)
-})
+    const regRex = new RegExp(req.params.searchTerms,"i")
+    const foods = await FoodModel.find({name:{$regex:regRex}})
+    
 
-router.get("/tags/:tagName",(req,res)=>{
-    const tagName = req.params.tagName;
-    const foods = sample_foods.filter(food => food.tags?.includes(tagName))
+   
     res.send(foods)
-})
+}))
 
-router.get("/:foodId",(req,res)=>{
-    const foodId =  req.params.foodId;
-    const food = sample_foods.find(food => food.id == foodId)
+
+router.get("/tags",ascyncHandler(async(req,res)=>{
+    const tags = await FoodModel.aggregate([
+        {
+            $unwind:"$tags"
+        },{
+            $group:{
+                _id:'$tags',
+                count:{$sum:1}
+            }
+        },{
+            $project:{
+                _id:0,
+                name:"$_id",
+                count:"$count"
+            }
+        }
+    ]).sort({count:-1})   //descending order
+
+
+    const all = {
+        name:"All",
+        count:await FoodModel.countDocuments()
+    }
+
+    tags.unshift(all)   //adds at the beginning of an tags
+    res.send(tags)
+}))
+
+router.get("/tags/:tagName",ascyncHandler(async(req,res)=>{
+    const foods = await FoodModel.find({tags:req.params.tagName})
+    res.send(foods)
+}))
+
+router.get("/:foodId",ascyncHandler(async(req,res)=>{
+    const food = await FoodModel.findById(req.params.foodId)
     res.send(food)
-})
+}))
 
 
 export default router
--- api/src/router/user.router.ts
+++ api/src/router/user.router.ts
@@ -1,20 +1,46 @@
 import Router from "express"
 import { sample_users } from "../data"
 import jwt from "jsonwebtoken"
+import ascyncHandler from "express-async-handler"
+import { UserModel } from "../models/user.model"
 
 const router = Router()
 
 
 
-router.post("/login",(req,res)=>{
-    const {email,password} = req.body
-    const user = sample_users.find(user => user.email === email && user.password === password)
-    if(user)
-    return res.send(generateTokenResponse(user))
-    else{
-        res.status(400).send("Email or password is not valid")
+router.get("/seed",ascyncHandler(
+    async(req,res)=>{
+        const foodCounts = await UserModel.countDocuments()
+
+        if(foodCounts>0)
+        {
+            res.send("Seed is already done")
+            return
+        }
+
+        await UserModel.create(sample_users)
+        res.send("Seed is done")
     }
-})
+  
+))
+
+
+
+router.post("/login",ascyncHandler(async(req,res)=>{
+    const {email,password}  = req.body;
+    const user = await UserModel.findOne({email,password})
+
+    if(user)
+    {
+        res.send(generateTokenResponse(user))
+
+    }else{
+        const BAD_REQUEST = 400;
+        res.status(BAD_REQUEST).send("User or password is invalid!");
+    }
+
+
+}))
 
 const generateTokenResponse = (user:any)=>{
 
